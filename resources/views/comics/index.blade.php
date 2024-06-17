3ab0ea57eaa9480b8dae63d15ed4ec5621b24e3a
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-06-17 15:49:38 +0200

    feat: added method destroy

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -18,11 +18,15 @@
                     <th scope="row">{{$comic['title']}}</th>
                     <td>{{$comic['series']}}</td>
                     <td>{{$comic['sale_date']}}</td>
-                    <td>
+                    <td class="d-flex gap-1">
                         <a class="btn btn-success" href="{{route('comics.show', ['comic' => $comic['id']])}}"><i class="fa-solid fa-info"></i></a>
                         <a class="btn btn-primary" href="{{route('comics.edit', ['comic' => $comic['id']])}}"><i class="fa-solid fa-pen-to-square"></i></a>
 
-
+                        <form action="{{route('comics.destroy', ['comic' => $comic['id']])}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+                        </form>
                         
                     </td>
                 </tr>
